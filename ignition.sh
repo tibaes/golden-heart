2340e268f283530042d051d5cb30aabc6337ea1f
Rafael Henrique Tibaes <r@fael.nl>
2016-09-01 18:26:40 -0300

    container names

--- ignition.sh
+++ ignition.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-container="ijulia"
-image="goldenheart"
+container="goldenheart"
+image="goldenheart_image"
 
 port_ijulia="9999"
 port_ssh="9990"
 
 alreadyBuilt=$(docker ps -a | grep ${container} | wc -l)
-if [ $alreadyBuilt -eq 0 ] 
+if [ $alreadyBuilt -eq 0 ]
 then
   # If hasn't, create a new one
   docker run -it -p ${port_ijulia}:9999 --name ${container} ${image}
 else
   # If already has ont, re start it
-  docker start -i ${container} 
+  docker start -i ${container}
 fi
