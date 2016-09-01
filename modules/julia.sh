176e0476b2075679d9cf1cac77bb98aaa3da7a01
Rafael Henrique Tibaes <r@fael.nl>
2016-09-01 18:10:55 -0300

    julia

---
+++ modules/julia.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aptitude update
+aptitude install -y julia
+
+echo "#!/bin/bash\njupyter notebook --no-browser --certfile=/root/mycert.pem --keyfile=/root/mykey.key" >> /root/ijulia.sh
+chmod +x /root/ijulia.sh
+
+julia -e 'Pkg.add("Images")'
+julia -e 'Pkg.add("ImageMagick")'
+julia -e 'Pkg.add("IJulia")'
+julia -e 'Pkg.build("IJulia")'
