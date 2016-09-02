27b2ece797fc63a26e25da0e0ea518167050db10
Rafael Henrique Tibaes <r@fael.nl>
2016-09-02 18:26:31 -0300

    fixed ignition

--- ignition.sh
+++ ignition.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 container="goldenheart"
-image="goldenheart_image"
+image="goldenheart"
+version="1.0.0"
 
 port_ijulia="9999"
-port_ssh="9990"
 
 alreadyBuilt=$(docker ps -a | grep ${container} | wc -l)
 if [ $alreadyBuilt -eq 0 ]
 then
   # If hasn't, create a new one
-  docker run -it -p ${port_ijulia}:9999 --name ${container} ${image}
+  docker run -d -it -p ${port_ijulia}:9999 --name ${container} ${image}:${version}
+  docker cp ~/.ssh ${container}:/root/
+  docker attach ${container}
+  echo "All done."
 else
   # If already has ont, re start it
   docker start -i ${container}
