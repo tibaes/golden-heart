a374f7f8d71f522342143c133b400548494ed9ab
Rafael Henrique Tibaes <r@fael.nl>
2016-09-13 13:32:53 -0300

    new source for wget opencv

--- ignition.sh
+++ ignition.sh
@@ -5,7 +5,7 @@ cd archive
 
 echo "Downloading DLib 19.1 and OpenCV 3.1.0"
 wget http://dlib.net/files/dlib-19.1.tar.bz2
-wget https://github.com/Itseez/opencv/archive/3.1.0.zip
+wget https://github.com/opencv/opencv/archive/3.1.0.zip
 
 echo "Download cuDNN v5.1 Runtime Library for Linux (Deb): libcudnn5_5.1.5-1+cuda8.0_amd64.deb"
 echo "Download cuDNN v5.1 Developer Library for Linux (Deb): libcudnn5-dev_5.1.5-1+cuda8.0_amd64.deb"
